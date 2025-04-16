69ffd91d130b5708eba561910c03f839d429b6a8
konradsoares <52551615+konradsoares@users.noreply.github.com>
2025-04-16 22:54:12 +0100

    Update loadbalancer.tf

--- terraform_blue_green/loadbalancer.tf
+++ terraform_blue_green/loadbalancer.tf
@@ -1,11 +1,16 @@
+variable "do_token" {}
+variable "active_droplet_id" {}
+variable "inactive_droplet_id" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
 resource "digitalocean_loadbalancer" "www_lb" {
   name   = "www-lb"
   region = "ams3"
 
-  droplet_ids = [
-    var.blue_droplet_id,
-    var.green_droplet_id
-  ]
+  droplet_ids = [var.active_droplet_id, var.inactive_droplet_id]
 
   forwarding_rule {
     entry_port     = 80
@@ -15,7 +20,7 @@ resource "digitalocean_loadbalancer" "www_lb" {
   }
 
   healthcheck {
-    port     = var.target_env == "blue" ? 80 : 81
+    port     = 9090 # Only the active node should respond to this
     protocol = "tcp"
   }
 }
