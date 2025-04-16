809391cf2ace77841048fa8d55c4fe4836f34f21
konradsoares <52551615+konradsoares@users.noreply.github.com>
2025-04-16 23:12:55 +0100

    Update variables.tf

--- terraform_blue_green/variables.tf
+++ terraform_blue_green/variables.tf
@@ -1,9 +1,1 @@
-variable "active_droplet_id" {
-  description = "Droplet ID to register on the load balancer"
-  type        = string
-}
-variable "active_deployment" {
-  description = "Which environment is active (blue or green)"
-  type        = string
-  default     = "blue"
-}
+
